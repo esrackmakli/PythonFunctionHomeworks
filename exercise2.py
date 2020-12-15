fec4240f2c9ccded17538ac12eb36b854b2b4faa
esrackmakli <68961319+esrackmakli@users.noreply.github.com>
2020-12-15 15:25:46 +0300

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -2,27 +2,20 @@
 
 birler = ["", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"]
 onlar = ["", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"]
-
+sayi = 56
 
 def sayiAtama(sayi):
     x = sayi
-    return print('okunus: ', sayiOkunusu(x))
+    return sayiOkunusu(x)
 
 
 def sayiOkunusu(sayi):
     birinci = sayi % 10
     ikinci = sayi // 10
-    return str(onlar[ikinci] + " " + birler[birinci])
+    return print("okunuus: " + str(onlar[ikinci] + " " + birler[birinci]))
 
 
-digits = 0
-sayi = 49
-
-while sayi > 0:
-    digits += 1
-    sayi //= 10
-
-if digits == 2:
+if sayi < 100 and sayi > 9:
     sayiAtama(sayi)
 else:
     print("please enter two digits number!")
